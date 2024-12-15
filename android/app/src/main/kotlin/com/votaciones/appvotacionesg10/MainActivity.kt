e9d51e8e74648642424e4e8b469b96349aa90190
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-12-15 12:24:38 -0500

    firste

---
+++ android/app/src/main/kotlin/com/votaciones/appvotacionesg10/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.votaciones.appvotacionesg10
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:appvotacionesg10/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
