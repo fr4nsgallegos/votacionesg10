d03507507e5f7c736154733fec1b5c3ea060466c
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-12-22 10:23:36 -0500

    navigator map page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:appvotacionesg10/pages/map_page.dart';
 import 'package:appvotacionesg10/utils/notification_service.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,24 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("VOTACIONES"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MapPage(),
+                  ),
+                );
+              },
+              child: Text("GO TO MAP"),
+            ),
+          ],
+        ),
       ),
     );
   }
