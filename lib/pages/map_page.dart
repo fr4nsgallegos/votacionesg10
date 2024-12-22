2240b7a371f52933aac111dbef737d19c8f2b78d
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-12-22 11:24:42 -0500

    map page init

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatelessWidget {
   @override
@@ -7,7 +8,14 @@ class MapPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Map Page"),
       ),
-      body: Column(),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            -18.00373470739627,
+            -70.2326096081443,
+          ),
+        ),
+      ),
     );
   }
 }
